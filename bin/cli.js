7e6f3b340ec485c0b50d64a1c29e59fa7f5097c3
osylvain <g.oliviersylvain@gmail.com>
2016-01-07 11:57:54 -0500

    package json

--- bin/cli.js
+++ bin/cli.js
@@ -2,8 +2,11 @@
 
 var program = require('commander');
 
+var path = require('path');
+var pkg = require( path.join(__dirname, '../package.json') );
+
 program
-  .version('0.1.0')
+  .version(pkg.version)
   .option('-u, --url [value]', 'url to scan')
   .parse(process.argv);
 
